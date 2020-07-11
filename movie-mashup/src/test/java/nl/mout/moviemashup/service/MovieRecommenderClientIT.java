abb4b53f651ba204b586b91387e63e4ef9968bf2
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-07-11 20:18:06 +0200

    Implement test for MovieRecommenderClient.

---
+++ movie-mashup/src/test/java/nl/mout/moviemashup/service/MovieRecommenderClientIT.java
@@ -0,0 +1,65 @@
+package nl.mout.moviemashup.service;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.MOCK;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@SpringBootTest(webEnvironment = MOCK)
+@ExtendWith({SpringExtension.class})
+class MovieRecommenderClientIT {
+
+    private WireMockServer wireMockServer;
+
+    @Autowired
+    private MovieRecommenderClient client;
+
+    @BeforeEach
+    void setUp() {
+        wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+    }
+
+    @AfterEach
+    void tearDown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    void testFindRecommendations() {
+        stubMovieRecommender();
+
+        Flux<String> recommendations = client.findRecommendations("skyfall");
+
+        assertRecommendations(recommendations, "The Bourne Legacy", "Casino Royale", "Mission: Impossible - Ghost Protocol", "One Small Hitch", "Zero Dark Thirty");
+    }
+
+    private void assertRecommendations(Flux<String> recommendations, String... expected) {
+        StepVerifier
+                .create(recommendations)
+                .expectNext(expected)
+                .expectComplete()
+                .verify();
+    }
+
+    private void stubMovieRecommender() {
+        wireMockServer.stubFor(get(urlPathEqualTo("/recommend"))
+                .withQueryParam("movieName", equalTo("skyfall"))
+                .willReturn(aResponse()
+                        .withStatus(OK.value())
+                        .withHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                        .withBodyFile("recommendations_response.json")));
+    }
+}
